3ba3f8dd54fc7a844aa15d28dfa484cca06dbff1
falgon <falgon53@yahoo.co.jp>
2017-07-30 00:00:26 +0900

    initial commit

---
+++ docs/js/imgheight.js
@@ -0,0 +1,9 @@
+dy(function () {
+	      wsize = $(window).width();
+	            $(".rokiicon").css("height", (wsize / 2.6) + "px");
+});
+    
+$(window).resize(function () {
+	     wsize = $(window).width();
+	          $(".rokiicon").css("height", (wsize / 2.6 ) + "px");
+});
